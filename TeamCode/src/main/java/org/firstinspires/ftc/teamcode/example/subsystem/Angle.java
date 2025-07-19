5a1874b9e471a23d54a16dd0e06bb65c37491ed5
GunnuCode <ganesh.attad@gmail.com>
2025-07-18 23:32:43 -0400

    7/18/25 software meeting, introduced with NextFTC

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/example/subsystem/Angle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/example/subsystem/Angle.java
@@ -7,6 +7,7 @@ import com.rowanmcalpin.nextftc.core.control.controllers.PIDFController;
 import com.rowanmcalpin.nextftc.core.control.controllers.feedforward.StaticFeedforward;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorEx;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorGroup;
+import com.rowanmcalpin.nextftc.ftc.hardware.controllables.RunToPosition;
 
 public class Angle extends Subsystem {
     // BOILERPLATE
@@ -18,9 +19,33 @@ public class Angle extends Subsystem {
     public MotorEx motorAngleLeft;
     public PIDFController controller = new PIDFController(10, 0.049988, 0.0, new StaticFeedforward(0.0));
     MotorGroup motorAngleGroup;
-    public Command toVertical{
-        return new
+    static class AngleTargets {
+        public static final int Low = 0;
+        public static final int Middle = 250;
+        public static final int High = 500;
     }
+
+    public Command toLow() {
+        return new RunToPosition(motorAngleGroup, // MOTOR TO MOVE
+                AngleTargets.Low, // TARGET POSITION, IN TICKS
+                controller, // CONTROLLER TO IMPLEMENT
+                this); // IMPLEMENTED SUBSYSTEM
+    }
+
+    public Command toMiddle() {
+        return new RunToPosition(motorAngleGroup, // MOTOR TO MOVE
+                AngleTargets.Middle, // TARGET POSITION, IN TICKS
+                controller, // CONTROLLER TO IMPLEMENT
+                this); // IMPLEMENTED SUBSYSTEM
+    }
+
+    public Command toHigh() {
+        return new RunToPosition(motorAngleGroup, // MOTOR TO MOVE
+                AngleTargets.High, // TARGET POSITION, IN TICKS
+                controller, // CONTROLLER TO IMPLEMENT
+                this); // IMPLEMENTED SUBSYSTEM
+    }
+
     public void initialize() {
         motorAngleLeft = new MotorEx("motorAngleLeft");
         motorAngleRight = new MotorEx("motorAngleRight");
